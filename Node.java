8dd4142c3d6fec382ad5e30584181255dc90eed9
ShitalKarad <shitalkarad24@gmail.com>
2023-05-23 19:26:41 +0530

    Added: Uc1 creating linked list

---
+++ Node.java
@@ -0,0 +1,17 @@
+package com.brigelabz;
+
+public class Node<T> {
+	 T key;
+	    Node<T> next;
+
+	    public Node() {
+
+	    }
+
+	    public Node(T key ) {
+	        this.key = key;
+
+	    }
+
+
+}
